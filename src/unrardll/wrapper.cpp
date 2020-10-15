37936b3778fd7aea1c788cf65b2cc7245bd6ed89
Norbert Preining <norbert@preining.info>
2020-10-15 23:30:32 +0900

    New upstream version 0.1.4

--- src/unrardll/wrapper.cpp
+++ src/unrardll/wrapper.cpp
@@ -133,12 +133,15 @@ write_all(const char* data, size_t sz, int fd) {
 #endif
         written = write(fd, data, sz);
         err = errno;
-        if (written >= sz) return true;
         if (written < 0) {
-            if (err == EINTR || err == EAGAIN || err == EWOULDBLOCK); continue;
+            if (err == EINTR || err == EAGAIN || err == EWOULDBLOCK)
+                continue;
             return false;
         }
-        if (written == 0 && err != 0) return false;
+        if ((size_t) written >= sz)
+            return true;
+        if (written == 0 && err != 0)
+            return false;
         sz -= written;
     } 
     return true;
